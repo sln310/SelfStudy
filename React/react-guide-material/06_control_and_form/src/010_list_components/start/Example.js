b377f36980d6209dff246cf6d8f2ab06c4b40c86
sh310310 <110559375+sh310310@users.noreply.github.com>
2023-04-19 10:56:41 -0700

    ✏️

--- React/react-guide-material/06_control_and_form/src/010_list_components/start/Example.js
+++ React/react-guide-material/06_control_and_form/src/010_list_components/start/Example.js
@@ -1,13 +1,16 @@
-
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
-
   return (
     <>
       <h3>配列の操作</h3>
       <ul>
-        
+        {/* <li>{animals[0]}</li>
+        <li>{animals[1]}</li>
+        <li>{animals[2]}</li> */}
+        {animals.map((animal) => (
+          <li key={animal}>Hello,{animal}</li>
+        ))}
       </ul>
     </>
   );
