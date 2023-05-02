ff723d50c6dc9da1447146883da64ab33ff292b5
sh310310 <110559375+sh310310@users.noreply.github.com>
2023-05-02 02:19:41 -0700

    portfolio

---
+++ React/react-portfolio/src/pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import { Inter } from "next/font/google";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Hanako Sato</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <section className=""></section>
+      </main>
+    </div>
+  );
+}
